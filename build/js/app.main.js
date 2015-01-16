cd18578209f36851ae435a0d31cb948fbb042ea0
wolfather <so.israelweb@gmail.com>
2015-01-16 20:09:17 -0200

    audio removed

--- build/js/app.main.js
+++ build/js/app.main.js
@@ -1,3 +1,4 @@
 
-var audio=document.querySelector('#audio'),visual=document.querySelector('#visual'),light,music={light:'../audio/deepershadeofsoul.mp3',dark:'../audio/letsgetiton.mp3'};window.addEventListener("devicelight",function(event){light=event.value;if(light<70){audio.src=music.dark;audio.play();visual.classList.add('oyeah');}
-else{audio.pause();visual.classList.remove('oyeah');}});
+var audio=document.querySelector('#audio'),visual=document.querySelector('#visual'),light,debug=document.querySelector('#debug'),music={light:'../audio/deepershadeofsoul.mp3',dark:'../audio/letsgetiton.mp3'};window.addEventListener("devicelight",function(event){light=event.value;console.log(light);debug.textContent=light;if(light<=30){console.log("dark",light);visual.toggleClass('dark');}
+if(ligth<=90){console.log('middle');visual.toggleClass('dim');}
+else{console.log("bright",light);visual.toggleClass('light');}});
--- dev/js/app.main.js
+++ dev/js/app.main.js
@@ -1,34 +1,43 @@
 var audio = document.querySelector('#audio'),
     visual = document.querySelector('#visual'),
     light,
-    //togglePlay = document.querySelector('#togglePlay'),
+    debug = document.querySelector('#debug'),
     music = {
         light : '../audio/deepershadeofsoul.mp3',
         dark : '../audio/letsgetiton.mp3'
     };
 
-//togglePlay.addEventListener('click', function(){
+
     window.addEventListener("devicelight", function (event) {
         light = event.value;
-        //console.log(light);
-
-        if(light < 70){
-            audio.src = music.dark;
-            audio.play();
-            //console.log("escuro", light);
-            visual.classList.add('oyeah');
+        console.log(light);
+        debug.textContent = light;
+        
+        if(light <= 30){
+            //audio.src = music.dark;
+            //audio.play();
+            
+            console.log("dark", light);
+            visual.toggleClass('dark');
+        }
+        if(ligth <= 90){
+            console.log('middle');
+            visual.toggleClass('dim');
         }
         else{
             //audio.src = music.light;
             //audio.play();
-            audio.pause();
-            //console.log("claro", light);
-            visual.classList.remove('oyeah');
+            //audio.pause();
+            
+            console.log("bright", light);
+            visual.toggleClass('light');
+            //visual.classList.remove('oyeah');
         }
 
     });
-    //return false;
 
+//togglePlay = document.querySelector('#togglePlay');
+//togglePlay.addEventListener('click', function(){
 //}, false);
 
 
